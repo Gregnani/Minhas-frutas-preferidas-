6617c0aa4030a4e159f4a7aec0e853617414cd7b
gregnani <fgregnani@gmail.com>
2023-03-03 22:40:00 -0300

    exercicio resolvido

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine(" --- Minhas frutas preferidas ---");
+Console.WriteLine("1- goiaba");
+Console.WriteLine("2- lichia");
+Console.WriteLine("3- morango");
+Console.WriteLine("4- uva");
+Console.WriteLine("5- pitaya");
